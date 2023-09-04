51f7e95a4d6cca1fdb5c33a54c336aa222f9e57d
Bhooker88 <139884664+Bhooker88@users.noreply.github.com>
2023-09-04 05:02:47 +0000

    first commit, may still update

--- src/app.js
+++ src/app.js
@@ -2,10 +2,62 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+  location.reload();
+});
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function cardOrder() {
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
+  const cards = [];
+  for (let s = 0; s < suits.length; s++) {
+    for (let n = 0; n < numbers.length; n++) {
+      const number = numbers[n];
+      const suit = suits[s];
+      cards.push({ number, suit });
+    }
+  }
+  return cards;
+}
+
+function randomCard(cards) {
+  const random = Math.floor(Math.random() * 51);
+  const cardNumber = cards[random].number;
+  const cardSuit = cards[random].suit;
+  let entity;
+  cardSuit === "Diamonds"
+    ? (entity = "&diams;")
+    : (entity = "&" + cardSuit.toLowerCase() + ";");
+  const card = document.createElement("div");
+  card.classList.add("card", cardSuit.toLowerCase());
+  card.innerHTML =
+    '<span class="card-suit top">' +
+    entity +
+    "</span>" +
+    '<span class="card-number">' +
+    cardNumber +
+    "</span>" +
+    '<span class="card-suit bot">' +
+    entity +
+    "</span>";
+
+  document.body.appendChild(card);
+}
+
+const cards = cardOrder();
+randomCard(cards);
